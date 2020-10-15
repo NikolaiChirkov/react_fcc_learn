5e72a1aa88619fcac9a56b6afb4bc387a90ed974
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-15 18:35:38 +0300

    first react component

---
+++ tutorial/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+
+  return <h4>This is first component</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
