3eaad5164b1ebae3f38420784dd877ef9e8e9213
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 04:03:30 +0300

    PropTypes - default value

--- react-advance/src/tutorial/10-prop-types/setup/Product.js
+++ react-advance/src/tutorial/10-prop-types/setup/Product.js
@@ -3,10 +3,12 @@ import propTypes from 'prop-types';
 import defaultImage from './../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+
   return <article className='product'>
-    <img src={image.url} alt={name}/>
-    <h4>{name}</h4>
-    <p>${price}</p>
+    <img src={url || defaultImage} alt={name || 'default name'}/>
+    <h4>{name || 'default name'}</h4>
+    <p>${price || 3.99}</p>
   </article>;
 };
 
@@ -14,12 +16,6 @@ Product.propTypes = {
   image: propTypes.object.isRequired,
   name: propTypes.string.isRequired,
   price: propTypes.number.isRequired
-}
-
-Product.defaultProps = {
-  name: 'default name',
-  price: 3.99,
-  image: defaultImage
 }
 
 export default Product;
---
+++ react-advance/src/tutorial/10-prop-types/setup/testing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const testing = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+testing.propTypes = {
+    
+}
+
+export default testing;
