7debef440ad1db36bd1106cef90c3d39e7a91ebc
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 03:58:07 +0300

    PropTypes - images

--- react-advance/src/tutorial/10-prop-types/setup/Product.js
+++ react-advance/src/tutorial/10-prop-types/setup/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import defaultImage from './../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
   return <article className='product'>
@@ -7,5 +9,17 @@ const Product = ({ image, name, price }) => {
     <p>${price}</p>
   </article>;
 };
+
+Product.propTypes = {
+  image: propTypes.object.isRequired,
+  name: propTypes.string.isRequired,
+  price: propTypes.number.isRequired
+}
+
+Product.defaultProps = {
+  name: 'default name',
+  price: 3.99,
+  image: defaultImage
+}
 
 export default Product;
