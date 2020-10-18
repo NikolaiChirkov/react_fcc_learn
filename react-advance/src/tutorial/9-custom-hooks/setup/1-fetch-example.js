74fbb3d21497be4273c095699d32a9fdd783cbf9
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 03:10:04 +0300

    Custom hooks - useFetch

--- react-advance/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ react-advance/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useFetch } from './2-useFetch';
 const url = 'https://course-api.netlify.app/api/javascript-store-products';
+
 const Example = () => {
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState([]);
+  const {loading, products} = useFetch(url);
 
-  const getProducts = async () => {
-    const response = await fetch(url);
-    const products = await response.json();
-    setProducts(products);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, [url]);
   console.log(products);
+
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
